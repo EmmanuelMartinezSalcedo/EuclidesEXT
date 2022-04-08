8cd20909a3e88e7a22743176bf3592bdc0337e79
EmmanuelMartinezSalcedo <90872703+EmmanuelMartinezSalcedo@users.noreply.github.com>
2022-04-07 21:52:17 -0500

    Update EuclidesEXT.py

--- EuclidesEXT.py
+++ EuclidesEXT.py
@@ -1,21 +1,21 @@
 def EuclidesEXT(n,m):
-    u0=1;u1=0;v0=0;v1=1
+    x0=1;x1=0;y0=0;y1=1
     if m==0:
-        return m,u0,v0
+        return m,x0,y0
     while m!=0:
         q=n//m
         r=n-m*q
-        u=u0-q*u1
-        v=v0-q*v1
+        x=x0-q*x1
+        y=y0-q*y1
         #Update a,b
         n=m
         m=r
         #Update for next iteration
-        u0=u1
-        u1=u
-        v0=v1
-        v1=v
-    return  n, u0, v0
+        x0=x1
+        x1=x
+        y0=y1
+        y1=y
+    return  n, x0, y0
 a=int(input())
 b=int(input())
 print(EuclidesEXT(a,b))
