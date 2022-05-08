a3b34795b0565ae04211d230f067b1020a628155
EmmanuelMartinezSalcedo <90872703+EmmanuelMartinezSalcedo@users.noreply.github.com>
2022-05-08 16:27:16 -0500

    Update EuclidesEXT.py

--- EuclidesEXT.py
+++ EuclidesEXT.py
@@ -1,19 +1,27 @@
 def EuclidesEXT(n,m):
-    x0=1;x1=0;y0=0;y1=1
     if m==0:
-        return m,x0,y0
-    while m!=0:
-        q=n//m
-        r=n%m
-        x=x0-q*x1
-        y=y0-q*y1
-        n=m
-        m=r
-        x0=x1
-        x1=x
-        y0=y1
-        y1=y
-    return  n, x0, y0
+        return (n,1,0)
+    else:
+        (d,x2,y2)=EuclidesEXT(m, n%m)
+        (x,y)=(y2,x2-(((n/m)-((n/m)%1))*y2))
+        return(d,x,y)
+
+def Diofantica(n,m,o,p):
+    (d,x0,y0)=EuclidesEXT(n,m)
+    d2=o/d
+    for i in range(0,p,1):
+        x1=(x0*d2)-(m/d)*i
+        y1=(y0*d2)-(n/d)*i
+        print("x1=",x1,",","y1=",y1)
 a=int(input())
 b=int(input())
+print("(MCD, X, Y)")
 print(EuclidesEXT(a,b))
+print("¿Desea encontrar la solución a una ecuacion diofantica?(y/n)")
+op=str(input())
+if op=='y':
+    print("Ingrese el resultado de la ecuacion diofantica")
+    c=int(input())
+    print("¿Cuantos posibles resultados quiere imprimir?")
+    d=int(input())
+    Diofantica(a,b,c,d)
